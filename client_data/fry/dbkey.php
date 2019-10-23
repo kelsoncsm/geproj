812e37ff76a9fa26dae24ea8540a15776e9b7a9a
Kelson Carlos <Kelson Carlos>
2019-10-23 19:01:43 -0300

    atualização banco

--- client_data/fry/dbkey.php
+++ client_data/fry/dbkey.php
@@ -2,20 +2,20 @@
 	include_once('GeProj/GeProj.php');
 	$dbkey = new stdClass();
 
-	// if(GeProj::server() == GeProj::SERVER_LOCAL){
+	 if(GeProj::server() == GeProj::SERVER_LOCAL){
 		$dbkey->DB_HOST = 'localhost';
-		$dbkey->DB_USER = 'root';
-		$dbkey->DB_PASS = 'vaiplaneta';
-		$dbkey->DB_BASE = 'geproj001';
+		$dbkey->DB_USER = "root";
+		$dbkey->DB_PASS = "vaiplaneta";
+		$dbkey->DB_BASE = "geproj001";
 		$dbkey->DB_PORT = null;
 		$dbkey->DB_SOCKET = null;
 		$dbkey->ID_EMPRESA = 1;
-	// } else {
-	// 	$dbkey->DB_HOST = 'geproj001.mysql.dbaas.com.br';
-	// 	$dbkey->DB_USER = 'geproj001';
-	// 	$dbkey->DB_PASS = 'FrYEng2019';
-	// 	$dbkey->DB_BASE = 'geproj001';
-	// 	$dbkey->DB_PORT = null;
-	// 	$dbkey->DB_SOCKET = null;
-	// 	$dbkey->ID_EMPRESA = 1;
-	// }
+	  } else {
+	  	$dbkey->DB_HOST = 'geproj001.mysql.dbaas.com.br';
+	  	$dbkey->DB_USER = 'geproj001';
+	  	$dbkey->DB_PASS = 'FrYEng2019';
+	  	$dbkey->DB_BASE = 'geproj001';
+	  	$dbkey->DB_PORT = null;
+	  	$dbkey->DB_SOCKET = null;
+	  	$dbkey->ID_EMPRESA = 1;
+	  }
